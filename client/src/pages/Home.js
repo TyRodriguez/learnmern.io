94b0194da0403f5c438701cb3c3883bea784911b
Ralph Magbanua <rmagbanua09@gmail.com>
2020-08-09 16:43:30 -0700

    new branch

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,14 +4,13 @@ import '../App.js';
 import NavBar from '../components/NavBar'
 import Iframe from '../components/iframe';
 // import LearnMernState from './components/stackblitzFrame/stackblitzFrame';
-import IframeResizer from 'iframe-resizer-react';
 import Tabs from '../components/Tabs'
 import Footer from '../components/Footer'
 
 
 function Home() {
   return (
-    <div className="App">
+    <div>
       <NavBar className="Nav"/>
       <Tabs />
       <Footer />
